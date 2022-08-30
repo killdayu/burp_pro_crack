318946efb4347698e9186b12ab8ae24a0a7818b0
killdayu <root@killdayu.com>
2022-08-30 17:48:28 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # burp_pro_crack
+
+一个小脚本，微不足道的贡献。
+
+burp_pro破解+汉化，macos专用，利用burp_pro自带的java环境。
+
+1. 下载burp_pro：https://portswigger.net/burp/releases#professional
+2. 安装burp_pro
+3. git clone https://github.com/killdayu/burp_pro_crack
+4. cd burp_pro_crack && ./burp_pro_crack.sh
+
+更新burp_pro的话，再次运行脚本即可，只有第一次安装需要激活。
+
+视频教程
+
+<video src="./burp_pro_crack.mp4"></video>
+
+Forked from https://github.com/WankkoRee/BurpLoaderKeygenCnF
---
+++ burp_pro_crack.sh
@@ -0,0 +1,9 @@
+#复制加载器
+cp "./BurpLoaderKeygenCnF.jar" "/Applications/Burp Suite Professional.app/Contents/Resources/app"
+#写入java启动参数
+echo "-noverify
+-javaagent:BurpLoaderKeygenCnF.jar=cn" >> "/Applications/Burp Suite Professional.app/Contents/vmoptions.txt"
+#绕过apple公证
+sudo xattr -rd com.apple.quarantine "/Applications/Burp Suite Professional.app" 
+#运行BurpLoaderKeygenCnF
+"/Applications/Burp Suite Professional.app/Contents/Resources/jre.bundle/Contents/Home/bin/java" -jar "/Applications/Burp Suite Professional.app/Contents/Resources/app/BurpLoaderKeygenCnF.jar"
